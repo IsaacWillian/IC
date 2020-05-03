7550e4b404d588101d34c914176ed0e865e12443
Isaac Willian <isaacwillian3@gmail.com>
2020-05-03 18:06:07 -0300

    Iniciado Tracking

--- Metodo_Calvo/Calvo.py
+++ Metodo_Calvo/Calvo.py
@@ -11,6 +11,22 @@ import pickle
 matplotlib.rcParams["backend"] = "Agg"
 from stentiford import stentiford
 
+# Separar dois grupos (terminações e bifurcaçoes) Checked
+# Separar caminhos que terminam TermTerm, BiTerm, BiBi 
+def tracking(img):
+    bifurcation_points = []
+    end_points = []
+    row_max,col_max = img.shape
+    for row in range(row_max):
+        for col in range(col_max):
+            if img[row,col] >= 150:
+                bifurcation_points.append((row,col))
+                print(row,col)
+            elif img[row,col] == 50:
+                print(row,col)
+                end_points.append((row,col))
+    return img
+
 '''Implementação do método sugerido por Calvo em Automatic detection and characterisation of retinal vessel
 tree bifurcations and crossovers in eye fundus images'''
 def Calvo_implementation(Image,Th,Tw):
@@ -38,58 +54,58 @@ def Calvo_implementation(Image,Th,Tw):
 
     num_rows,num_cols = L.shape
 
-    Image_copy = np.copy(Image)
+    Image = np.copy(Image)
 
     for row in range(num_rows):
         for col in range(num_cols):
             label =  L[row,col]
             if label in comp2remove:
-                Image_copy[row,col] = 0
+                Image[row,col] = 0
 
 
-    # Image_copy = ndi.median_filter(Image_copy,3)
+    # Image = ndi.median_filter(Image,3)
     for i in range(4):
-        Image_copy = morph.binary_dilation(Image_copy)
+        Image = morph.binary_dilation(Image)
 
     for i in range(4):
-        Image_copy = morph.binary_erosion(Image_copy)
+        Image = morph.binary_erosion(Image)
     
 
-    Image_copy = Image_copy.astype(np.int16)
-    Image_copy = stentiford(Image_copy)
+    Image = Image.astype(np.int16)
+    Image = stentiford(Image)
+
     
 
     #Percorre os vizinhos fazendo a somatória ds seu valores
-    Image_labels = Image_copy * 255
-    Image_labels = skicolor.gray2rgb(Image_labels)
-    row_max, col_max = Image_copy.shape
+    Image_sum = Image.copy()
+    row_max, col_max = Image.shape
     for row in range(row_max):
         for col in range(col_max): 
-            if Image_copy[row,col] == 1:
+            if Image[row,col] == 1:
                 sum = 0
                 x_ant = x_add[-1]
                 y_ant = y_add[-1]
                 for x,y in zip(x_add,y_add):
-                    sum += abs(Image_copy[row + x_ant, col+y_ant] - Image_copy[row+x,col+y])
+                    sum += abs(Image[row + x_ant, col+y_ant] - Image[row+x,col+y])
                     x_ant = x
                     y_ant = y
 
                 sum *= 0.5
-
-                if sum > 2 :
-                    rr,cc = skidraw.circle_perimeter(row,col,5)
-                    Image_labels[rr,cc] = (255,0,0)
+                Image_sum[row,col] = int(sum*50)
+                
                    
 
+    tracking(Image_sum)
 
-    plt.imshow(Image_labels)
+    plt.imshow(Image_sum,"gray")
     plt.show()
-    return Image_copy
+    return Image
 
 
-Image_original = plt.imread("/home/isaacw/Documentos/repos/DRIVE/training/images/21_training.tif") 
-mask = plt.imread("/home/isaacw/Documentos/repos/DRIVE/training/mask/21_training_mask.gif")
+Image_original = plt.imread("../Imagens/21_training.tif") 
+mask = plt.imread("../Imagens/21_training_mask.gif")
 mask = morph.binary_erosion(mask)
+
 
 Image_obtida = Calvo_implementation(Image_original,95,90)
 # Image_obtida = np.bitwise_and(Image_obtida,mask)
