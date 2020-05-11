24d1a085f722e30a8a57aec302853d99fcc8f4db
Isaac Willian <isaacwillian3@gmail.com>
2020-05-11 13:16:46 -0300

    Troca de SO

--- Metodo_Calvo/Calvo.py
+++ Metodo_Calvo/Calvo.py
@@ -11,6 +11,28 @@ import pickle
 matplotlib.rcParams["backend"] = "Agg"
 from stentiford import stentiford
 
+class Branch:
+    def __init__(self,initial_point,path,path_len):
+        self.initial_point = initial_point
+        self.path = path
+        self.path_len = path_len
+
+def SearchNeighbor(img,point,previous_point):
+    x_add = [0,1,0,-1]
+    y_add = [-1,0,1,0]
+    for x,y in zip(x_add,y_add):
+        neighbor = (point[0]+x,point[1]+y)
+        if img[neighbor] != 0 and ((neighbor) != previous_point):
+            return (point[0]+x,point[1]+y)
+
+def ShowBranchs(img,path):
+    img_rgb = skicolor.gray2rgb(img)
+    for x,y in path:
+        img_rgb[x,y] = [255,0,0]    
+
+    plt.imshow(img_rgb)
+    plt.show()
+
 # Separar dois grupos (terminações e bifurcaçoes) Checked
 # Separar caminhos que terminam TermTerm, BiTerm, BiBi 
 def tracking(img):
@@ -21,10 +43,37 @@ def tracking(img):
         for col in range(col_max):
             if img[row,col] >= 150:
                 bifurcation_points.append((row,col))
-                print(row,col)
             elif img[row,col] == 50:
-                print(row,col)
                 end_points.append((row,col))
+    
+    for end_point in end_points:
+        path = [end_point]
+        branchs = []
+        path_len = 1
+        fim = 0
+        previous_point = (0,0)
+        while fim != 1:
+            neighbor = SearchNeighbor(img,end_point,previous_point)
+            path_len +=1
+            path.append((neighbor))
+            if neighbor in end_points:
+                fim = 1
+            elif neighbor in bifurcation_points:
+                branch = Branch(end_point,path,path_len)
+                branchs.append(branch)
+                ShowBranchs(img,path)
+                fim = 1
+                if path_len < 10:
+                    path.pop(-1)
+                    for x,y in path:
+                        img[x,y] = 0
+                plt.imshow(img,"gray")
+                plt.show()
+            else: 
+                previous_point = end_point
+                end_point = neighbor
+
+        
     return img
 
 '''Implementação do método sugerido por Calvo em Automatic detection and characterisation of retinal vessel
