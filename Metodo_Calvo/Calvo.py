906c60e447a705d5dad2b487374ed985b5adba0a
IsaacWillian <isaacwillian3@gmail.com>
2020-06-23 09:59:00 -0300

    Terminado a analise topologica

--- Metodo_Calvo/Calvo.py
+++ Metodo_Calvo/Calvo.py
@@ -19,10 +19,15 @@ class Branch:
 def SearchNeighbor(img,point,previous_point):
     x_add = [0,1,0,-1]
     y_add = [-1,0,1,0]
+    neighbors = []
     for x,y in zip(x_add,y_add):
-        neighbor = (point[0]+x,point[1]+y)
+        _x = point[0]+x
+        _y = point[1]+y
+        #import pdb;pdb.set_trace() 
+        neighbor = (_x,_y) 
         if img[neighbor] != 0 and ((neighbor) != previous_point):
-            return (point[0]+x,point[1]+y)
+            neighbors.append(neighbor) 
+    return neighbors
 
 def ShowBranchs(img,path):
     img_rgb = skicolor.gray2rgb(img)
@@ -32,9 +37,7 @@ def ShowBranchs(img,path):
     plt.imshow(img_rgb)
     plt.show()
 
-# Separar dois grupos (terminações e bifurcaçoes) Checked
-# Separar caminhos que terminam TermTerm, BiTerm, BiBi 
-def tracking_and_remove_branch(img):
+def get_features_points(img):
     bifurcation_points = []
     end_points = []
     row_max,col_max = img.shape
@@ -42,48 +45,219 @@ def tracking_and_remove_branch(img):
         for col in range(col_max):
             if img[row,col] >= 150:
                 bifurcation_points.append((row,col))
-            elif img[row,col] == 50:
+            if img[row,col] == 50:
                 end_points.append((row,col))
-    
-    for end_point in end_points:
-        path = [end_point]
-        branchs = []
-        path_len = 1
-        fim = 0
-        previous_point = (0,0)
-        while fim != 1:
-            neighbor = SearchNeighbor(img,end_point,previous_point)
-            path_len +=1
-            path.append((neighbor))
-            if neighbor in end_points:
-                fim = 1
-            elif neighbor in bifurcation_points:
-                branch = Branch(end_point,path,path_len)
-                branchs.append(branch)
-                fim = 1
-                if path_len < 10: ## Threshold
-                    bifurcation = path.pop(-1)
-                    img[bifurcation] = img[bifurcation] - 50
-                    for x,y in path:
-                        img[x,y] = 0
-            else: 
-                previous_point = end_point
-                end_point = neighbor
 
-        
+    
+    return bifurcation_points,end_points
+
+def tracking_and_remove_branchs(img):
+    bifurcations,ends = get_features_points(img)
+    
+    for end_point in ends:
+        point=end_point
+        fim=0
+        previous_point = (-1,-1)
+        path = [point]
+        path_len = 1
+        while fim != 1:
+                neighbor = SearchNeighbor(img,point,previous_point)[0]
+                path_len +=1
+                path.append(neighbor)
+                if neighbor in bifurcations:
+                    fim = 1
+                    branch = Branch(end_point,path,path_len)
+                    img= remove_branch(img,branch)                                            
+                        
+                else: 
+                    previous_point = point
+                    point = neighbor
+    
+    return img
+
+def create_graph(img):
+    bifurcations,ends = get_features_points(img)
+    features_points = bifurcations + ends
+
+    node_index = 0
+    map_pos_to_index = {}
+    for feature_point in features_points:
+        map_pos_to_index[feature_point] = node_index
+        node_index += 1 
+
+    
+    graph = [ [] for i in range (len(map_pos_to_index))]
+    for feature_point in features_points:
+        fim=0
+        previous_point = (-1,-1)
+        neighbors = SearchNeighbor(img,feature_point,previous_point)
+        for point in neighbors:
+            previous_point = feature_point
+            init_point = point
+            fim = 0
+            while fim != 1:
+                neighbor = SearchNeighbor(img,point,previous_point)[0]
+                if neighbor in features_points:
+                    fim = 1
+                    index_node_1 = map_pos_to_index[feature_point]
+                    index_node_2 = map_pos_to_index[neighbor]
+                    if index_node_2 not in graph[index_node_1]: 
+                        graph[index_node_1].append(index_node_2)
+                        graph[index_node_2].append(index_node_1)
+                        
+                        
+                else: 
+                    previous_point = point
+                    point = neighbor
+    
+    
+    return img,graph,map_pos_to_index
+
+
+def remove_branch(img,branch):
+    if branch.path_len < 10: ## Threshold
+        bifurcation = branch.path.pop(-1)
+        end_point = branch.path[0] 
+        #Apenas é removido se o track começa no ponto de terminação e chega em bifurcação
+        if img[bifurcation]>=150 and img[end_point]==50: 
+            img[bifurcation] = img[bifurcation] - 50
+            for x,y in branch.path:
+                img[x,y] = 0
+              
     return img
 
 def local_analysis_classification(img,radius,p):
+    crossover_points = []
+    bifurcation_points = []
+    img_bin = img >= 50 #Para binarizar a imagem
     R1 = radius - p
     R2 = radius
     R3 = radius + p
 
-    # Aplicar formulas passando por todos os pixeis acima de 150, verificando se são B ou F
-    # for
-        # for
-            #funçao 
+    row_max,col_max = img.shape
+    for row in range(row_max):
+        for col in range(col_max):
+            if img[row,col] >= 150: 
+                point = (row,col)  #bifurcações ou crossover 
 
-    pass
+                C = 2 * Cros(img_bin,point,R1) + Cros(img_bin,point,R2) + Cros(img_bin,point,R3)
+                B = Bifur(img_bin,point,R1) + Bifur(img_bin,point,R2) + Bifur(img_bin,point,R3) * 2
+
+                if C > B:
+                    img[row,col] = 200
+                    crossover_points.append((row,col))
+                else:
+                    bifurcation_points.append((row,col))
+                    img[row,col] = 150   
+    
+    ShowPoints(img)
+    print("Crossover Poinst = ",crossover_points)
+    print("Bifurcation Points = ",bifurcation_points)
+    return img,crossover_points,bifurcation_points
+
+
+def Cros(img,point,R):
+    row,col = point
+    rr,cc = skidraw.circle_perimeter(row,col,R)
+    result = np.sum(img[rr,cc])
+    if result == 3: 
+        return 0
+    else: 
+        return 1  
+    
+
+def Bifur(img,point,R):
+    row,col = point
+    rr,cc = skidraw.circle_perimeter(row,col,R)
+    result = np.sum(img[rr,cc])
+    if result == 3: 
+        return 1
+    else:
+        return 0   
+   
+
+def local_analysis(img,point,R):
+    row,col = point
+    rr,cc = skidraw.circle_perimeter(row,col,R)
+    result = np.sum(img[rr,cc])
+    if result == 3: 
+        return 1
+    elif result == 4:
+        return 0
+
+def topological_analysis(img,crossover_points,radius,graph,map_pos_to_index):
+    pairs = find_pairs(crossover_points,radius)
+    pairs = pairs_is_connected(pairs,graph,map_pos_to_index)
+    ShowPairs(img,pairs)
+
+    return img
+
+def pairs_is_connected(pairs,graph,map_pos_to_index):
+    for pair in pairs:
+        index_node_1 = map_pos_to_index[pair[0]]
+        index_node_2 = map_pos_to_index[pair[1]]
+
+        if index_node_1 not in graph[index_node_2] or index_node_2 not in graph[index_node_1]:
+            pairs.remove(pair)
+
+    return pairs
+
+def ShowPairs(img,pairs):
+    img_rgb = img.copy()
+    img_rgb = skicolor.gray2rgb(img)
+    for pair in pairs:
+        m = ((pair[1][0]+pair[0][0])//2, (pair[1][1]+pair[0][1])//2) 
+        rr,cc = skidraw.circle_perimeter(m[0],m[1],10)
+        img_rgb[rr,cc] = [0,255,0]
+        rr,cc = skidraw.line(m[0],m[1],pair[0][0],pair[0][1])
+        img_rgb[rr,cc] = [0,0,255]
+        rr,cc = skidraw.line(m[0],m[1],pair[1][0],pair[1][1])
+        img_rgb[rr,cc] = [255,0,0]
+    
+    plt.imshow(img_rgb)
+    plt.show()
+
+def find_pairs(crossover_points,radius):
+    pairs = []
+    no_more_pairs = False
+    while (no_more_pairs == False):
+        d_smaller = radius * 2
+        no_more_pairs = True
+        for point in crossover_points:
+            for point_aux in crossover_points:
+                if point != point_aux :
+                    d = np.sqrt((point_aux[0]-point[0])**2 + (point_aux[1]-point[1])**2 )
+                    if d < d_smaller:
+                        no_more_pairs = False
+                        d_smaller = d
+                        pair = ((point,point_aux))
+        if no_more_pairs == False:
+            crossover_points.remove(pair[0])
+            crossover_points.remove(pair[1])
+            pairs.append(pair)
+
+    return pairs
+
+
+
+def ShowPoints(img):  # e perimetros  print
+    img_rgb = img.copy()
+    img_rgb = skicolor.gray2rgb(img)
+    row_max,col_max = img.shape
+    for row in range(row_max):
+        for col in range(col_max):
+            if img[row,col] == 150:
+                rr,cc = skidraw.circle(row,col,5)
+                img_rgb[rr,cc] = [255,0,0]
+            elif img[row,col] == 200:
+                rr,cc = skidraw.circle(row,col,5)
+                img_rgb[rr,cc] = [0,255,0]
+
+    plt.imshow(img_rgb)
+    plt.show()
+
+
+    
 
 '''Implementação do método sugerido por Calvo em Automatic detection and characterisation of retinal vessel
 tree bifurcations and crossovers in eye fundus images'''
@@ -102,7 +276,7 @@ def Calvo_implementation(Image,Th,Tw):
     Th = np.percentile(Image_array,Th)
     #  Tw_zip = zip(percentile_array,Tw)
     #  print(list(Tw_zip))
-    #  print(Th)
+
     Image = skifilter.apply_hysteresis_threshold(Image,Tw,Th)
 
     L,N = ndi.label(Image)
@@ -139,7 +313,7 @@ def Calvo_implementation(Image,Th,Tw):
     row_max, col_max = Image.shape
     for row in range(row_max):
         for col in range(col_max): 
-            if Image[row,col] == 1:
+            if Image[row,col] == 1:                  
                 sum = 0
                 x_ant = x_add[-1]
                 y_ant = y_add[-1]
@@ -151,12 +325,15 @@ def Calvo_implementation(Image,Th,Tw):
                 sum *= 0.5
                 Image_sum[row,col] = int(sum*50)
                 
-                   
+             
 
-    tracking_and_remove_branch(Image_sum)
-
-    plt.imshow(Image_sum,"gray")
-    plt.show()
+    Rc = 10
+    p = 5
+    img = tracking_and_remove_branchs(Image_sum)
+    img,graph,map_pos_to_index = create_graph(img)
+    img,crossover_points,bifurcation_points = local_analysis_classification(img,Rc,p)
+    img = topological_analysis(img,crossover_points,Rc,graph,map_pos_to_index)
+    
     return Image
 
 
@@ -168,5 +345,3 @@ mask = morph.binary_erosion(mask)
 Image_obtida = Calvo_implementation(Image_original,95,90)
 # Image_obtida = np.bitwise_and(Image_obtida,mask)
 
-plt.imshow(Image_obtida, 'gray')
-plt.show()
